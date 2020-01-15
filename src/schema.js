1554077cb82039df1886e421c21a5b5dd30d633d
vapvin <tpehdj12@daum.net>
2020-01-15 20:05:32 +0900

    api GraphQL

---
+++ src/schema.js
@@ -0,0 +1,18 @@
+import path from 'path';
+import { makeExecutableSchema } from 'graphql-tools';
+import {
+  fileLoader,
+  metgeResolvers,
+  mergeTypes,
+  mergeResolvers
+} from 'merge-graphql-schemas';
+
+const allTypes = fileLoader(path.join(__dirname, '/api/**/*.graphql'));
+const allResolvers = fileLoader(path.join(__dirname, 'api/**/*.js'));
+
+const schema = makeExecutableSchema({
+  typeDefs: mergeTypes(allTypes),
+  resolvers: mergeResolvers(allResolvers)
+});
+
+export default schema;
