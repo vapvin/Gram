ca0ac2bc475565dd1ebe65c17b8fa564e027aeda
vapvin <tpehdj12@daum.net>
2020-01-16 23:24:43 +0900

    Edit

--- src/utils.js
+++ src/utils.js
@@ -27,7 +27,7 @@ export const sendSecretMail = (address, secret) => {
     from: "vins@gram.com",
     to: address,
     subject: "Login Secret for Gram",
-    hyml: `Hello! Your login secret it <b>?${secret}</b> <br />Copy paste on the app/website to log in`
+    hyml: `Hello! Your login secret is <strong>?${secret}</strong> <br />Copy paste on the app/website to log in`
   };
   return sendMail(email);
 };
