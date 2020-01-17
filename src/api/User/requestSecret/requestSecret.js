4f7d36d8dff3746e31cd7b3a1513c3287742f5d6
vapvin <tpehdj12@daum.net>
2020-01-17 19:36:25 +0900

    toggle Like Api

--- src/api/User/requestSecret/requestSecret.js
+++ src/api/User/requestSecret/requestSecret.js
@@ -6,13 +6,11 @@ export default {
     requestSecret: async (_, args) => {
       const { email } = args;
       const loginSecret = generateSecret();
-      console.log(loginSecret);
       try {
         await sendSecretMail(email, loginSecret);
         await prisma.updateUser({ data: { loginSecret }, where: { email } });
         return true;
-      } catch (error) {
-        console.log(error);
+      } catch {
         return false;
       }
     }
