2431e177521b7358a6c3c1630d113e69b1fb7e1f
vapvin <tpehdj12@daum.net>
2020-01-17 17:54:36 +0900

    Authorization

--- src/api/User/confirmSecret/confirmSecret.js
+++ src/api/User/confirmSecret/confirmSecret.js
@@ -3,7 +3,7 @@ import { generateToken } from '../../../utils';
 
 export default {
   Mutation: {
-    confirmSecret: async (_, args) => {
+    confirmSecret: async (_, args, { request }) => {
       const { email, secret } = args;
       const user = await prisma.user({ email });
       if (user.loginSecret === secret) {
--- src/server.js
+++ src/server.js
@@ -1,17 +1,17 @@
 import './env';
 
 import { GraphQLServer } from 'graphql-yoga';
-import passport from 'passport';
 import logger from 'morgan';
 import schema from './schema';
 import './passport';
+import { authenticateJwt } from './passport';
 
 const PORT = process.env.PORT || 4000;
 
-const server = new GraphQLServer({ schema });
+const server = new GraphQLServer({ schema, context: request => ({ request }) });
 
 server.express.use(logger('dev'));
-server.express.use(passport.authenticate('jwt'));
+server.express.use(authenticateJwt);
 
 server.start({ port: PORT }, () =>
   console.log(`Server Running on http://localhost:${PORT}`)
