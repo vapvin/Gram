0508ec236ba427170b3b12a52189dc0aa0f34e76
vapvin <tpehdj12@daum.net>
2020-01-25 16:30:53 +0900

    Fixed Error and Edit Fragment

--- src/api/Post/seeFullPost/seeFullPost.js
+++ src/api/Post/seeFullPost/seeFullPost.js
@@ -1,23 +1,11 @@
 import { prisma } from '../../../../generated/prisma-client';
+import { FULL_POST_FRAGMENT } from '../../../fragments';
 
 export default {
   Query: {
     seeFullPost: async (_, args) => {
       const { id } = args;
-      const post = await prisma.post({ id });
-      const comment = await prisma
-        .post({ id })
-        .comments()
-        .$fragment(COMMENT_FRAGMENT);
-      const likeCount = await prisma
-        .likesConnection({ where: { post: { id } } })
-        .aggregate()
-        .count();
-      return {
-        post,
-        comment,
-        likeCount
-      };
+      return await prisma.post({ id }).$fragment(FULL_POST_FRAGMENT);
     }
   }
 };
--- src/fragments.js
+++ src/fragments.js
@@ -1,8 +1,39 @@
+export const USER_FRAGMENT = `
+ 
+        id
+        username
+    
+`;
+
 export const COMMENT_FRAGMENT = `
-fragment CommentParts on Comment {
+
     id
     text
     user {
-        username
-    }
+        ${USER_FRAGMENT}
+    
 }`;
+
+export const FILE_FRAGMENT = `
+
+        id
+        url
+    
+`;
+
+export const FULL_POST_FRAGMENT = `
+    fragment PostParts on Post {
+        id
+        location
+        caption
+        file {
+            ${FILE_FRAGMENT}
+        }
+        comment {
+            ${COMMENT_FRAGMENT}
+        }
+        user {
+            ${USER_FRAGMENT}
+        }
+    }
+`;
