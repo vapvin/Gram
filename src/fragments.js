f84f2959cc1e5a7835ba85c420c49ac5141cee76
vapvin <tpehdj12@daum.net>
2020-02-03 18:23:45 +0900

    pull post fragment changed

--- src/fragments.js
+++ src/fragments.js
@@ -28,22 +28,22 @@ export const MESSAGE_FRAGMENT = `
     }
 `;
 
-export const FULL_POST_FRAGMENT = `
-    fragment PostParts on Post{
-        id
-        location
-        caption
-        files {
-            ${FILE_FRAGMENT}
-        }
-        comments {
-            ${COMMENT_FRAGMENT}
-        }
-        user {
-            ${USER_FRAGMENT}
-        }
-    }
-`;
+// export const FULL_POST_FRAGMENT = `
+//     fragment PostParts on Post{
+//         id
+//         location
+//         caption
+//         files {
+//             ${FILE_FRAGMENT}
+//         }
+//         comments {
+//             ${COMMENT_FRAGMENT}
+//         }
+//         user {
+//             ${USER_FRAGMENT}
+//         }
+//     }
+// `;
 
 export const ROOM_FRAGMENT = `
     fragment RoomParts on Room {
