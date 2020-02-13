bce5fdd8d361a87279eeaa20a5177903f1124414
vapvin <tpehdj12@daum.net>
2020-02-13 21:04:00 +0900

    Me Query is not defined

--- src/api/User/me/me.js
+++ src/api/User/me/me.js
@@ -2,15 +2,10 @@ import { prisma } from '../../../../generated/prisma-client';
 
 export default {
   Query: {
-    me: async (_, __, { request, isAutheticated }) => {
-      isAutheticated(request);
+    me: async (_, __, { request, isAuthenticated }) => {
+      isAuthenticated(request);
       const { user } = request;
-      const userProfile = prisma.user({ id: user.id });
-      const posts = prisma.user({ id: user.id }).posts();
-      return {
-        user: userProfile,
-        posts
-      };
+      return await prisma.user({ id: user.id });
     }
   }
 };
